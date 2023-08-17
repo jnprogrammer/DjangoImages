0023bb41559f05152afccfce1d5a5071f19e244d
Josh <joshuanparker@gmail.com>
2023-08-17 11:26:36 -0400

    Finished image upload using admin

--- djangoimages/models.py
+++ djangoimages/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class Movie(models.Model):
     name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='djangoimages/files/covers')
+    image = models.ImageField(upload_to='djangoimages/img')
--- djangoimages/settings.py
+++ djangoimages/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_ROOT = 'djangoimages/files/covers'
+MEDIA_URL = ''
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
